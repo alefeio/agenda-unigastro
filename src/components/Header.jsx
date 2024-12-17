752ec0d0979cd5e812aef7ea1242368cc4da8465
Alexandre Feio <alefeio@gmail.com>
2024-12-17 16:51:50 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <h1>UniGastro Pará</h1>
+        </header>
+    )
+}
